53f440af9766aad1611c2b8bcac36c99a9383fa4
zhangxuhui1992 <1083606942@qq.com>
2020-06-24 11:31:23 +0800

    BUS rbbitmq全局刷新搭建

--- cloud-config-clinet3366/src/main/java/com/atguigu/springcloud/ConfigClientMain3366.java
+++ cloud-config-clinet3366/src/main/java/com/atguigu/springcloud/ConfigClientMain3366.java
@@ -7,13 +7,12 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 /**
  * @author zhangxuhui
  * @email zxh_1633@163.com
- * @create 2020-06-23 13:26
+ * @create 2020-06-24 10:43
  */
 @SpringBootApplication
 @EnableEurekaClient
-public class ConfigClientMain3355 {
+public class ConfigClientMain3366 {
     public static void main(String[] args) {
-        SpringApplication.run( ConfigClientMain3355.class,args);
+        SpringApplication.run(ConfigClientMain3366.class, args);
     }
 }
-
---
+++ cloud-config-clinet3366/src/main/java/com/atguigu/springcloud/controller/ClientController.java
@@ -0,0 +1,24 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhangxuhui
+ * @email zxh_1633@163.com
+ * @create 2020-06-24 10:44
+ */
+@RestController
+@RefreshScope
+public class ClientController {
+
+    @Value("${myapplcation}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
